927fe477a29e184bd04f6317e7cc2fb8999c43b7
Yahir-Juarez <idv22c.yjuarez@uartesdigitales.edu.mx>
2025-01-25 17:28:23 -0600

    Cambios

--- Assets/Yahir/Scripts/ManagerSelection.cs
+++ Assets/Yahir/Scripts/ManagerSelection.cs
@@ -7,16 +7,31 @@ using UnityEngine.InputSystem;
 public class ManagerSelection : MonoBehaviour
 {
     [SerializeField]
-    private List<Image> m_listImageBackGround = new List<Image>();
+    private GameObject m_dataPlayersBackground;
+
+    private List<GameObject> m_listImageBackGround = new List<GameObject>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         GameManager.Instance.playerInputManager.enabled = true;
+        GameManager.Instance.playerInputManager.EnableJoining();
+
+        if (m_dataPlayersBackground == null) { return; }
+
+        //foreach (GameObject child in m_dataPlayersBackground)
+        //{
+        //    m_listImageBackGround.Add(child.gameObject);
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void newPlayer()
+    {
+
     }
 }
