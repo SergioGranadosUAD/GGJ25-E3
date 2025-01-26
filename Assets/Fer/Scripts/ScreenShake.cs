098d209f02694a2134870644c2a0073cfc692bd2
FerFran2004 <ldv22c.ffranyutti@uartesdigitales.edu.mx>
2025-01-25 18:19:43 -0600

    ScreenShake Script

--- Assets/Fer/Scripts/ScreenShake.cs
+++ Assets/Fer/Scripts/ScreenShake.cs
@@ -2,15 +2,62 @@ using UnityEngine;
 
 public class ScreenShake : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static ScreenShake instance;
+
+    Camera MainCameraRef;
+
+    [SerializeField]
+    float shakeDur = 0f;
+    [SerializeField]
+    float shakeMag = 0.6f;
+    [SerializeField]
+    float damingSpeed = 1f;
+
+    Vector3 initialPos;
+
+
+
+    public void Awake()
+    {
+        instance = this;
+        MainCameraRef = GetComponent<Camera>();
+    }
+
+
+    private void OnEnable()
+    {
+        initialPos = transform.localPosition;
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (shakeDur > 0) 
+        {
+            transform.localPosition = initialPos + Random.insideUnitSphere * shakeMag;
+            shakeDur -= Time.deltaTime * damingSpeed;
+        }
+        else
+        {
+            shakeDur = 0f;
+            transform.localPosition = initialPos;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Shake();
+        }
+
+    }
+
+
+    public void Shake()
+    {
+        shakeDur = .1f;
     }
 }
